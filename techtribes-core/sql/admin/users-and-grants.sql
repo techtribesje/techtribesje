0673d144e95b00d50120838fd899376a699f8330
Simon Brown <simon.brown@codingthearchitecture.com>
2013-08-11 23:21:28 +0100

    Modified MySQL create schema script.

--- techtribes-core/sql/admin/users-and-grants.sql
+++ techtribes-core/sql/admin/users-and-grants.sql
@@ -1,5 +1,6 @@
 CREATE USER 'techtribesje'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, LOCK_TABLES ON techtribesje.* to 'techtribesje'@'localhost';
 
+CREATE SCHEMA `techtribesje_test` DEFAULT CHARACTER SET utf8;
+CREATE USER 'test'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON techtribesje_test.* to 'test'@'localhost' IDENTIFIED BY 'password';
-
