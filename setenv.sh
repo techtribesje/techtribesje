05b877aa201254e9c8cacf39e7de22464d34d331
Simon Brown <simon.brown@codingthearchitecture.com>
2013-11-06 08:49:05 +0000

    Moved Ant on my local dev environment.

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,3 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_10.jdk/Contents/Home/
-export ANT_HOME=~/dev/apache-ant-1.9.1
+export ANT_HOME=~/Documents/dev/apache-ant-1.9.1
 export PATH=$ANT_HOME/bin:$PATH
