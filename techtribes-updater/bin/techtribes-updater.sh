9359b67d76dd830138e40400b24834a410f35330
Simon Brown <simon.brown@codingthearchitecture.com>
2014-02-01 10:16:08 +0000

    Command line apps now share a lib directory.

--- techtribes-updater/bin/techtribes-updater.sh
+++ techtribes-updater/bin/techtribes-updater.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dtwitter4j.http.useSSL=true -cp ./lib/*:. je.techtribes.updater.Main
+java -Dtwitter4j.http.useSSL=true -cp ./lib/*:../lib/*:. je.techtribes.updater.Main
--- techtribes-util/bin/techtribes-util.sh
+++ techtribes-util/bin/techtribes-util.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp ./lib/*:. je.techtribes.util.Main $1
+java -cp ./lib/*:../lib/*:. je.techtribes.util.Main $1 $2
