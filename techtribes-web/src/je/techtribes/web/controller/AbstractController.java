a364a34ce27fc0a19d3e925da00c79823527f854
Simon Brown <simon.brown@codingthearchitecture.com>
2014-08-08 23:19:22 +0100

    Removed most active people section.

--- techtribes-web/src/je/techtribes/web/controller/AbstractController.java
+++ techtribes-web/src/je/techtribes/web/controller/AbstractController.java
@@ -1,6 +1,5 @@
 package je.techtribes.web.controller;
 
-import je.techtribes.component.activity.ActivityComponent;
 import je.techtribes.component.contentsource.ContentSourceComponent;
 import je.techtribes.component.log.LoggingComponent;
 import je.techtribes.domain.ContentSource;
@@ -13,16 +12,10 @@ public class AbstractController {
 
     protected LoggingComponent loggingComponent;
     protected ContentSourceComponent contentSourceComponent;
-    protected ActivityComponent activityComponent;
 
     @Autowired
     public void setContentSourceComponent(ContentSourceComponent contentSourceComponent) {
         this.contentSourceComponent = contentSourceComponent;
-    }
-
-    @Autowired
-    public void setActivityComponent(ActivityComponent activityComponent) {
-        this.activityComponent = activityComponent;
     }
 
     @Autowired
@@ -34,15 +27,9 @@ public class AbstractController {
         model.addAttribute("peopleAndTribes", contentSourceComponent.getPeopleAndTribes());
         model.addAttribute("techTribes", contentSourceComponent.getContentSources(ContentSourceType.Tech));
 
-        model.addAttribute("activityListForPeople", activityComponent.getActivityListForPeople());
-        model.addAttribute("activityListForBusinessTribes", activityComponent.getActivityListForBusinessTribes());
-        model.addAttribute("activityListForCommunityTribes", activityComponent.getActivityListForCommunityTribes());
-
         if (SecurityContextHolder.getContext().getAuthentication() != null) {
             model.addAttribute("user", lookupContentSourceBySignedInTwitterId());
         }
-
-        model.addAttribute("includeVideos", false);
     }
 
     protected void setPageTitle(ModelMap model, String... titles) {
