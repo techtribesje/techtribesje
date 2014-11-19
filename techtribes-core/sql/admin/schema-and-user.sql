b7ac38f1bc885a3f0452dfe2289a5bb72cd6a56f
Simon Brown <simon.brown@codingthearchitecture.com>
2014-11-19 23:49:07 +0000

    Removed localhost restriction.

--- techtribes-core/sql/admin/schema-and-user.sql
+++ techtribes-core/sql/admin/schema-and-user.sql
@@ -1,2 +1,2 @@
 CREATE SCHEMA `techtribesje` DEFAULT CHARACTER SET utf8;
-GRANT SELECT, INSERT, UPDATE, DELETE, LOCK_TABLES ON techtribesje.* to 'techtribesje'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, LOCK_TABLES ON techtribesje.* to 'techtribesje'@'%' IDENTIFIED BY 'password';
